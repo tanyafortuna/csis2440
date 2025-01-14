8851739cbd1ace7c284d8ef8d72a2a8436efbdff
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-01-14 13:44:19 -0700

    Move index.php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSIS2440</title>
+  </head>
+  <body>
+    <h1>This is Tanya Fortuna's PHP page</h1>
+    <p>Someday, this page will be an awesome page!</p>
+    <a href="index.php">This link will help Jeff grade this assignment</a> :0]
+</body>
+</html>
