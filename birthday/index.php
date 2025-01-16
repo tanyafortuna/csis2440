0f0affae45ea192ad8739af1bab10fafb7fd99b9
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-01-16 15:16:04 -0700

    Add birthday project

---
+++ birthday/index.php
@@ -0,0 +1,17 @@
+<?php
+  error_reporting(-1);
+  ini_set( 'display_errors', 1 );
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Birthday project</title>
+</head>
+<body>
+  <?php
+    echo '<h1>Happy birthday!</h1>';
+    echo '<img src="img/maggie.jpg" width="400px">';
+  ?>
+</body>
+</html>
