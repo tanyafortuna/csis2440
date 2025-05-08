d50c6dd12a84b4e420245ddd54cfd4476c4053bf
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-05-08 16:24:41 -0600

    Update product cards to pull from DB

--- catalog/includes/product-card.php
+++ catalog/includes/product-card.php
@@ -1,14 +1,22 @@
 <?php
-  function generateProductCard($id, $name, $price) {
+  function generateProductCard($id) {
+    $product = getProductFromDB($id);
+
     $output = "";
 
     $output .= '<a class="product-card" href="product.php?id='.$id.'">';
     $output .= '<div class="product-img">';
-    $output .= '<img src="img/product-images/'.$id.'.jpg">';
+    $output .= '<img src="';
+    $output .= $product['image'];
+    $output .= '">';
     $output .= '</div>';
     $output .= '<div class="product-details">';
-    $output .= '<p class="product-name">'.$name.'</p>';
-    $output .= '<p class="product-price">$'.$price.'</p>';
+    $output .= '<p class="product-name">';
+    $output .= $product['name'];
+    $output .= '</p>';
+    $output .= '<p class="product-price">$';
+    $output .= number_format($product['price'], 2);
+    $output .= '</p>';
     $output .= '</div>';
     $output .= '</a>';
 
