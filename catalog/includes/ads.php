bffa489c9b89f8e30d6ab8bd56233c2d34c73e94
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-05-11 15:37:26 -0600

    Remove ad from name of imgs (ad-blocker issues)

--- catalog/includes/ads.php
+++ catalog/includes/ads.php
@@ -4,9 +4,9 @@
   echo '<h1 class="common">SHOP ALL<img class="icon" src="img/icons/arrow-right.png"></h1>';
   echo '</a>';
   echo '<div id="ads-images">';
-  echo '<a class="ad-link" href="product.php?id=13"><img class="ad-img" src="img/ad-iron-carrot.webp"></a>';
-  echo '<a class="ad-link" href="product.php?id=14"><img class="ad-img" src="img/ad-unicycle.jpg"></a>';
-  echo '<a class="ad-link" href="product.php?id=12"><img class="ad-img" src="img/ad-instant-girl.webp"></a>';
+  echo '<a class="ad-link" href="product.php?id=13"><img class="ad-img" src="img/iron-carrot.webp"></a>';
+  echo '<a class="ad-link" href="product.php?id=14"><img class="ad-img" src="img/unicycle.jpg"></a>';
+  echo '<a class="ad-link" href="product.php?id=12"><img class="ad-img" src="img/instant-girl.webp"></a>';
   echo '</div>';
   echo '</section>';
 ?>
