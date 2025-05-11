7a9c8849c11982779abe7e37ca6d605bfdcc6329
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-05-10 21:22:32 -0600

    Restyle highlighted product on home page

--- catalog/includes/index/highlighted.php
+++ catalog/includes/index/highlighted.php
@@ -1,14 +1,14 @@
 <?php
-  echo '<section id="highlighted">';
+  echo '<section id="highlighted" class="common">';
   echo '<h1 class="common">NOW AVAILABLE</h1>';
-  echo '<div id="highlighted-product">';
+  echo '<div id="highlighted-product" class="common subsection">';
   echo '<img id="iron-carrot-ad" src="img/ad-iron-carrot.webp">';
   echo '<img id="newspaper" src="img/newspaper.webp">';
-  echo '<p id="iron-carrot-tagline">Not your garden-variety gadget!</p>';
-  echo '<p id="review-1">“Aerodynamic and slightly crunchy. What more could you want?” - Business Insider</p>';
-  echo '<p id="review-2">“It\'s not safe, it\'s not quiet, and it\'s definitely not FDA-approved - but wow, is it fun.” - Loyal ACME Customer</p>';
+  echo '<p id="review-1">"It\'s not your garden-variety gadget!"<BR>- Popular Mechanics</p>';
+  echo '<p id="review-2">“Aerodynamic and slightly crunchy. What more could you want?” - Business Insider</p>';
+  echo '<p id="review-3">“It\'s not safe, it\'s not quiet, and it\'s definitely not FDA-approved - but wow, is it fun.” - Loyal ACME Customer</p>';
   echo '<p id="iron-carrot-blurb">';
-  echo '<span>Introducing the Iron Carrot</span>™ — ACME Corporation\'s latest breakthrough in high-performance produce-inspired tech. Engineered for speed, impact, and questionable practicality, this sleek, carrot-shaped innovation is already turning heads (and breaking windows) across the country. Whether you\'re chasing, fleeing, or just want to make an impression at the local canyon edge, the Iron Carrot delivers unstoppable reliability with signature ACME style. Limited quantities available — get yours today!';
+  echo '<span>Introducing the Iron Carrot</span>™ — ACME Corporation\'s latest breakthrough in high-performance produce-inspired tech. Designed for speed, impact, and questionable practicality, this sleek, carrot-shaped innovation is already turning heads (and breaking windows) across the country. Whether you\'re chasing, fleeing, or just want to make an impression at the local canyon edge, the Iron Carrot delivers unstoppable reliability with signature ACME style. Limited quantities available — get yours today!';
   echo '</p>';
   echo '<div id="buy-now" class="button-container">';
   echo '<a href="product.php?id=13">BUY NOW</a>';
