1546e9e2b645dff49581dbbae767539dfa83aa1a
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-05-11 16:16:44 -0600

    Remove "ads" from html (caught by blocker)

--- catalog/includes/ads.php
+++ catalog/includes/ads.php
@@ -1,12 +1,12 @@
 <?php
-  echo '<section id="ads" class="common">';
+  echo '<section id="all-products" class="common">';
   echo '<a id="shop-all" href="catalog.php">';
   echo '<h1 class="common">SHOP ALL<img class="icon" src="img/icons/arrow-right.png"></h1>';
   echo '</a>';
-  echo '<div id="ads-images">';
-  echo '<a class="ad-link" href="product.php?id=13"><img class="ad-img" src="img/iron-carrot.webp"></a>';
-  echo '<a class="ad-link" href="product.php?id=14"><img class="ad-img" src="img/unicycle.jpg"></a>';
-  echo '<a class="ad-link" href="product.php?id=12"><img class="ad-img" src="img/instant-girl.webp"></a>';
+  echo '<div id="a-images">';
+  echo '<a class="a-link" href="product.php?id=13"><img class="a-img" src="img/iron-carrot.webp"></a>';
+  echo '<a class="a-link" href="product.php?id=14"><img class="a-img" src="img/unicycle.jpg"></a>';
+  echo '<a class="a-link" href="product.php?id=12"><img class="a-img" src="img/instant-girl.webp"></a>';
   echo '</div>';
   echo '</section>';
 ?>
