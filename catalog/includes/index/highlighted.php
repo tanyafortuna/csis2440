74d39e1112f650236be75f987a76b73774fdcd1e
Tanya Fortuna <tanyafortuna24@gmail.com>
2025-05-11 19:23:34 -0600

    Tweak CSS files (mostly spacing)

--- catalog/includes/index/highlighted.php
+++ catalog/includes/index/highlighted.php
@@ -6,7 +6,7 @@
   echo '<img id="newspaper" src="img/newspaper.webp" alt="A newspaper open to a page saying Acme\'s Best Product Ever">';
   echo '<p id="review-1">"It\'s not your garden-variety gadget!"<BR>- Popular Mechanics</p>';
   echo '<p id="review-2">“Aerodynamic and slightly crunchy. What more could you want?” - Business Insider</p>';
-  echo '<p id="review-3">“It\'s not safe, it\'s not quiet, and it\'s definitely not FDA-approved - but wow, is it fun.” - Loyal ACME Customer</p>';
+  echo '<p id="review-3">“It\'s not safe, it\'s not quiet, and it\'s definitely not FDA-approved - but wow,<br>is it fun.” - Loyal ACME Customer</p>';
   echo '<p id="iron-carrot-blurb">';
   echo '<span>Introducing the Iron Carrot</span>™ — ACME Corporation\'s latest breakthrough in high-performance produce-inspired tech. Designed for speed, impact, and questionable practicality, this sleek, carrot-shaped innovation is already turning heads (and breaking windows) across the country. Whether you\'re chasing, fleeing, or just want to make an impression at the local canyon edge, the Iron Carrot delivers unstoppable reliability with signature ACME style. Limited quantities available — get yours today!';
   echo '</p>';
